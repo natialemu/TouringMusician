ade1713ac33bc160f7db600509fc4bc2da25a318
Nathnael Alemu <nathnael.alemu@yahoo.com>
2017-09-07 20:01:19 -0500

    cleaned up code by removing extra comments and unused imports

--- app/src/main/java/com/google/engedu/touringmusician/CircularLinkedList.java
+++ app/src/main/java/com/google/engedu/touringmusician/CircularLinkedList.java
@@ -1,17 +1,4 @@
-/* Copyright 2016 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 
 package com.google.engedu.touringmusician;
 
@@ -25,11 +12,7 @@ public class CircularLinkedList implements Iterable<Point> {
     private class Node {
         Point point;
         Node prev, next;
-        /**
-         **
-         **  YOUR CODE GOES HERE
-         **
-         **/
+
     }
 
     Node head;
@@ -49,11 +32,6 @@ public class CircularLinkedList implements Iterable<Point> {
 
         }else{
 
-            //new node's next needs to point to previous first
-            //new node's prev needs to point to the last element
-            //previous first's prev needs to point to new node
-            //head needs to point to new no
-            //last element
             newNode.next = head.next;//new node
             head.next = newNode;//head points to new node
 
--- app/src/main/java/com/google/engedu/touringmusician/TourMap.java
+++ app/src/main/java/com/google/engedu/touringmusician/TourMap.java
@@ -1,17 +1,4 @@
-/* Copyright 2016 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 
 package com.google.engedu.touringmusician;
 
@@ -28,7 +15,6 @@ import android.view.View;
 import android.widget.TextView;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 public class TourMap extends View {
@@ -52,74 +38,53 @@ public class TourMap extends View {
         canvas.drawBitmap(mapImage, 0, 0, null);
         Paint pointPaint = new Paint();
         pointPaint.setColor(Color.RED);
-        /**
-         **
-         **  YOUR CODE GOES HERE
-         **
-         **/
+
         List<Point> points = new ArrayList<>();
         List<Point> secondPoints = new ArrayList<>();
         List<Point> thirdPoints = new ArrayList<>();
-        populatePoints(secondPoints,secondList);
-        populatePoints(thirdPoints,thirdList);
+        populatePoints(secondPoints, secondList);
+        populatePoints(thirdPoints, thirdList);
         for (Point p : mainList) {
             points.add(p);
-            /**
-             **
-             **  YOUR CODE GOES HERE
-             **
-             **/
+
             canvas.drawCircle(p.x, p.y, 20, pointPaint);
         }
 
-        for(int i = 0; i < points.size(); i++)
-        {
-            if(i == points.size() - 1){
-                canvas.drawLine(points.get(i).x, points.get(i).y,points.get(0).x,points.get(0).y,pointPaint);
-            }else {
+        for (int i = 0; i < points.size(); i++) {
+            if (i == points.size() - 1) {
+                canvas.drawLine(points.get(i).x, points.get(i).y, points.get(0).x, points.get(0).y, pointPaint);
+            } else {
                 canvas.drawLine(points.get(i).x, points.get(i).y, points.get(i + 1).x, points.get(i + 1).y, pointPaint);
             }
         }
 
-        if(insertMode.equals("Compare all")){
+        if (insertMode.equals("Compare all")) {
 
             pointPaint.setColor(Color.BLACK);
-            for(int i = 0; i < secondPoints.size(); i++)
-            {
-                if(i == secondPoints.size() - 1){
-                    canvas.drawLine(secondPoints.get(i).x, secondPoints.get(i).y,secondPoints.get(0).x,secondPoints.get(0).y,pointPaint);
-                }else {
+            for (int i = 0; i < secondPoints.size(); i++) {
+                if (i == secondPoints.size() - 1) {
+                    canvas.drawLine(secondPoints.get(i).x, secondPoints.get(i).y, secondPoints.get(0).x, secondPoints.get(0).y, pointPaint);
+                } else {
                     canvas.drawLine(secondPoints.get(i).x, secondPoints.get(i).y, secondPoints.get(i + 1).x, secondPoints.get(i + 1).y, pointPaint);
                 }
             }
 
             pointPaint.setColor(Color.GREEN);
-            for(int i = 0; i < thirdPoints.size(); i++)
-            {
-                if(i == thirdPoints.size() - 1){
-                    canvas.drawLine(thirdPoints.get(i).x, thirdPoints.get(i).y,thirdPoints.get(0).x,thirdPoints.get(0).y,pointPaint);
-                }else {
+            for (int i = 0; i < thirdPoints.size(); i++) {
+                if (i == thirdPoints.size() - 1) {
+                    canvas.drawLine(thirdPoints.get(i).x, thirdPoints.get(i).y, thirdPoints.get(0).x, thirdPoints.get(0).y, pointPaint);
+                } else {
                     canvas.drawLine(thirdPoints.get(i).x, thirdPoints.get(i).y, thirdPoints.get(i + 1).x, thirdPoints.get(i + 1).y, pointPaint);
                 }
             }
 
         }
 
-        /**
-         **
-         **  YOUR CODE GOES HERE
-         **
-         **/
     }
 
     private void populatePoints(List<Point> points, CircularLinkedList mainList) {
         for (Point p : mainList) {
             points.add(p);
-            /**
-             **
-             **  YOUR CODE GOES HERE
-             **
-             **/
 
         }
 
@@ -129,14 +94,14 @@ public class TourMap extends View {
     public boolean onTouchEvent(MotionEvent event) {
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
-                Point p = new Point((int) event.getX(), (int)event.getY());
+                Point p = new Point((int) event.getX(), (int) event.getY());
                 if (insertMode.equals("Closest")) {
                     mainList.insertNearest(p);
                 } else if (insertMode.equals("Smallest")) {
                     mainList.insertSmallest(p);
-                } else if(insertMode.equals("Beginning")) {
+                } else if (insertMode.equals("Beginning")) {
                     mainList.insertBeginning(p);
-                }else{
+                } else {
                     mainList.insertNearest(p);
                     secondList.insertSmallest(p);
                     thirdList.insertBeginning(p);
